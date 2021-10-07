19133414b44858ae2cf365cff5dc8913fa317c9a
boritjjaroo <boritjjaroo@gmail.com>
2021-10-07 21:27:50 +0900

    주소 검색 기능 추가

--- naver/map.py
+++ naver/map.py
@@ -85,6 +85,57 @@ def coords_to_addr(longitude, latitude):
 
     return address
 
+
+# 네이버맵에서 주소 검색 URL
+# 원본 : https://map.naver.com/v5/api/search?caller=pcweb&query=경상남도 양산시 동면 금산리 1434-12번지&type=all&searchCoord=129.02129840000026;35.311907900000186&page=1&displayCount=20&isPlaceRecommendationReplace=true&lang=ko
+api_url_addr_search = 'https://map.naver.com/v5/api/search?query='
+
+header = {
+    #accept: application/json, text/plain, */*
+    #accept-encoding: gzip, deflate, br
+    #accept-language: ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4
+    #cache-control: no-cache
+    #content-type: application/json
+    #cookie: NNB=RFKYOAM5OONGA; NID_JKL=pk8hbv+ycy6XdDoCQm2jt/ANAbywxe46P9x+rkcWmu8=; NID_AUT=6xLSgMNx8xu9+yr5HfYDa/d2FPEnM8bQ8YE5xHP40wdpElok3UyNXmzPKe0+KCIq; NFS=2; page_uid=hgkOwlpw8DNssdTv5Jwssssssss-099643; csrf_token=3cd8c9bafd7c327bc818b12fb42b3077bf76aa9a9ec5bd0699a8167c84398e911f36c132f78a2179b049d8a5db4e7e73867634dae21e673f2b7f22ba25c19ce2; BMR=s=1633494397471&r=https%3A%2F%2Fm.blog.naver.com%2Fhankrah%2F221755651815&r2=https%3A%2F%2Fwww.google.com%2F; NID_SES=AAABluSux5x2u7q/ETBJLkInIKRSVHahbwKTHgBsasxH+Yr6V05K5wMkYBYGzzJuIGWDdGLgsPmtYXxKZ4dwLwG0s+sHLGs6g53kY9SMZXo+ZFoa1/6bJ3fS/wXbz5zRdk3bhSJhvFn5sACvCCaXk5doWdZMsNFOP0N5E8p4JWrQh17Y4NpE5z+c2hfZO5jn0ONAFeW4DNFeA39kEHR7cM8rOq2TO8RnTiIXaabOuc7CZrjPrwuDHlMPCWW5LvQ/h+qQ9Q6Vk5J/+kGt/zyGf997e7APV4zDiklV0ZmRZWYFQGlq8T30slPhQUCjfEePKNYpNX+Ld427LJCJAlApVSgfn0/gNTZy7tBlgY/YMdoqkxbLu9wnv4dTDBuXm2v+2xT7nns/8bB8kPdEDRLy6qBqncrbuTUPOGJNEu/rwHtkaYtf8y1+JStUa/9w4noiAl+YD7HyNpBvQJLdfHEODwSsKhMwbgXgNXqIMKpt0kKKHzEFhspLHGfVeURYfhvzDo+ZfSDvcF0gzRWH3jwt3b7GbEeQbupTgOlvfCtki3vJddDB; page_uid=3850d49c-04eb-4187-8277-13c7e6d5bc7a
+    #expires: Sat, 01 Jan 2000 00:00:00 GMT
+    #pragma: no-cache
+    'referer': 'https://map.naver.com/',
+    #sec-ch-ua: "Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"
+    #sec-ch-ua-mobile: ?0
+    #sec-ch-ua-platform: "Windows"
+    #sec-fetch-dest: empty
+    #sec-fetch-mode: cors
+    #sec-fetch-site: same-origin
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+}
+
+class AddressInfo:
+    def __init__(self):
+        self.address_road = ''
+
+    def __str__(self) -> str:
+        return str(self.address_road)
+
+def addr_search(search_address):
+    url = f'{api_url_addr_search}{search_address}'
+    response = requests.get(url, headers=header)
+
+    if response.status_code != 200 :
+        print(f'Http error : {response.status_code}')
+        return None
+
+    #response 데이터 확인
+    #print(response.content)
+
+    json_data = json.loads(response.content)
+    #print(json_data)
+
+    info = AddressInfo()
+    address_list = json_data['result']['address']['jibunsAddress']['list']
+    info.address_road = address_list[0]['mappedAddress']['fullAddress']
+
+    return info
+
 # IP로 위치 찾기
 # https://map.naver.com/v5/api/gloc?ip=211.213.235.44&ntype=Y&ext=y&ncodetype=all
 api_url_ip_to_addr_format = 'https://map.naver.com/v5/api/gloc?ip={0}&ntype=Y&ext=y&ncodetype=all'
@@ -118,5 +169,6 @@ def ip_to_addr(ip_str):
 
 
 if __name__ == "__main__":
+    print(addr_search('경상남도 양산시 동면 금산리 1424-11번지'))
     print(coords_to_addr('128.3660622', '36.1432817'))
     print(ip_to_addr('211.213.235.44'))
